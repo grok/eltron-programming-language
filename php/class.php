41ff87dfb7af3da4b9339d9766fd678a050d7982
Sterling Hamilton <sterling.hamilton+git-commit@gmail.com>
2012-04-19 10:25:51 -0700

    Improving how string for ASCII is constructed. Adding new methods for generator barcodes. Added an example for barcode generation.

--- php/class.php
+++ php/class.php
@@ -24,23 +24,89 @@
 			return $this->_get_end_of_line() . 'N' . $this->_get_end_of_line();
 		}
 
-		public function create_label() {
-			return $this->_get_new_label();
-		}
-
 		protected function _get_print( $count = 1 ) {
 			return 'P' . $count . $this->_get_end_of_line();
 		}
 
-		public function print_label( $count = 1 ) {
-			return $this->_get_print( $count );
+		protected function _get_ascii() {
+			return 'A';
+		}
+
+		protected function _get_barcode() {
+			return 'B';
+		}
+
+		public function set_x_axis( $dots ) {
+			return $dots;
+		}
+
+		public function set_y_axis( $dots ) {
+			return $dots;
+		}
+
+		public function set_rotation( $value ) {
+			return $value;
+		}
+
+		public function set_font( $value = 0 ) {
+			return $value;
+		}
+
+		public function set_x_expansion( $multiplier ) {
+			return $multiplier;
+		}
+
+		public function set_y_expansion( $multiplier ) {
+			return $multiplier;
+		}
+
+		public function set_reverse( $boolean ) {
+			return $boolean ? 'R' : 'N';
+		}
+
+		public function set_data( $value ) {
+			return '"' . $value . '"';
+		}
+
+		public function set_type( $type ) {
+			return $type;
+		}
+
+		public function set_narrow_bar_width( $dots ) {
+			return $dots;
+		}
+
+		public function set_wide_bar_width( $dots ) {
+			return $dots;
+		}
+
+		public function set_height( $dots ) {
+			return $dots;
+		}
+
+		public function set_show_text( $boolean ) {
+			return $boolean ? 'B' : 'N';
 		}
 
 		# Problem: Too many damn parameters.
 		# Emotion: http://i.imgur.com/KGAk4.gif
 		# Solution: Not apparent yet.
 		public function add_ascii_text( $x_axis, $y_axis, $rotation, $font, $expand_x, $expand_y, $reverse, $data ) {
-			return "A$x_axis,$y_axis,$rotation,$font,$expand_x,$expand_y,$reverse,\"$data\"" . $this->_get_end_of_line();
+			$parameters = compact( 'x_axis', 'y_axis', 'rotation', 'font', 'expand_x', 'expand_y', 'reverse', 'data' );
+			return $this->_get_ascii() . implode( ',', $parameters ) . $this->_get_end_of_line();
+		}
+
+		public function add_barcode( $x_axis, $y_axis, $rotation, $type, $narrow_bar_width, $wide_bar_width, $height, $show_text, $data ) {
+			$parameters = compact( 'x_axis', 'y_axis', 'rotation', 'type', 'narrow_bar_width', 'wide_bar_width', 'height', 'show_text', 'data' );
+			return $this->_get_barcode() . implode( ',', $parameters ) . $this->_get_end_of_line();
+		}
+
+		public function create_label() {
+			return $this->_get_new_label();
+		}
+
+		public function print_label( $count = 1 ) {
+			return $this->_get_print( $count );
 		}
 
 	}
--- php/example.php
+++ php/example.php
@@ -4,7 +4,27 @@
 	$epl = new EltronProgrammingLanguage();
 	$epl->initialize();
 	echo $epl->create_label();
-	echo $epl->add_ascii_text(0,0,0,0,5,5,'N','TEST!');
+	echo $epl->add_ascii_text(
+		$epl->set_x_axis( 0 ),
+		$epl->set_y_axis( 0 ),
+		$epl->set_rotation( 0 ),
+		$epl->set_font( 0 ),
+		$epl->set_x_expansion( 5 ),
+		$epl->set_y_expansion( 5 ),
+		$epl->set_reverse( true ),
+		$epl->set_data( 'TEST!' )
+	);
+	echo $epl->add_barcode(
+		$epl->set_x_axis( 0 ),
+		$epl->set_y_axis( 0 ),
+		$epl->set_rotation( 0 ),
+		$epl->set_type( 3 ),
+		$epl->set_narrow_bar_width( 0 ),
+		$epl->set_wide_bar_width( 0 ),
+		$epl->set_height( 100 ),
+		$epl->set_show_text( true ),
+		$epl->set_data( 'TEST!' )
+	);
 	echo $epl->print_label();
 
 ?>
